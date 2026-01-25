5ae25a2f81f791673b646f8ee72eb718b9f39293
Lucas Bucht <lmb290@pitt.edu>
2026-01-25 14:27:22 -0500

    Added string constructor and destructor

--- project1/project1/BitArray.cpp
+++ project1/project1/BitArray.cpp
@@ -27,11 +27,32 @@ BitArray::BitArray(intmax_t size) {
 }
 
 BitArray::BitArray(const std::string & value) {
-    // TODO
+    m_size = 0;
+    m_bits = nullptr;
+    m_good = true;
+
+    if (value.empty()){
+        m_good = false;
+        return;
+    }
+
+    m_size = static_cast<intmax_t>(value.size());
+    m_bits = new bool[m_size];
+
+    for (intmax_t i = 0; i < m_size; i++){
+        char c = value[m_size - i - 1];
+        if (c == '0')
+            m_bits[i] = false;
+        else if (c == '1')
+            m_bits[i] = true;
+        else
+            m_good = false;
+            return;
+    }
 }
 
 BitArray::~BitArray() {
-    // TODO
+    delete[] m_bits;
 }
 
-// TODO: other methods
+
