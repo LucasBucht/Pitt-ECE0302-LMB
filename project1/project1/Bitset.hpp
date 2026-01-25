d077a2c2d7a8f2c1eb1b2a2f35ec6c8b109fc810
Lucas Bucht <lmb290@pitt.edu>
2026-01-25 14:57:09 -0500

    Added javadoc comments to Bitset.hpp

--- project1/project1/Bitset.hpp
+++ project1/project1/Bitset.hpp
@@ -8,25 +8,61 @@ class Bitset{
 
     virtual ~Bitset(){};
     
-    // TODO COMMENT
+    /**
+     * Returns the number of bits in the bitset.
+     * 
+     * @return the size of the bitset
+     */
     virtual intmax_t size() const = 0;
     
-    // TODO COMMENT
+    /**
+     * Returns whether the bitset is in a valid state.
+     * 
+     * @return true if the bitset is valid, otherwise false
+     */
     virtual bool good() const = 0;
     
-    // TODO COMMENT
+    /**
+     * Sets the bit at the given index to 1.
+     * If index is out of range, bitset becomes invalid.
+     * 
+     * @param index the index of the bit to set
+     */
     virtual void set(intmax_t index) = 0;
     
-    // TODO COMMENT
+    /**
+     * Resets the bit at the given index to 0.
+     * If index is out of range, bitset becomes invalid.
+     * 
+     * @param index the index of the bit to reset
+     */
     virtual void reset(intmax_t index) = 0;
     
-    // TODO COMMENT
+    /**
+     * Toggles the bit at the given index.
+     * If index is out of range, bitset becomes invalid.
+     * 
+     * @param index the index of the bit to toggle
+     */
     virtual void toggle(intmax_t index) = 0;
 
-    // TODO COMMENT
+    /**
+     * Tests whether the bit at the given index is set.
+     * Returns false if the bit is not set or index is out of range.
+     * If index is out of range, bitset becomes invalid.
+     * 
+     * @param index the index of the bit to test
+     * @return true if the bit is set, otherwise false
+     */
     virtual bool test(intmax_t index) = 0;
 
-    // TODO COMMENT
+    /**
+     * Returns a string representation of the bitset.
+     * String is ordered from most significant bit to least significant bit.
+     * Uses characters '1' and '0'.
+     * 
+     * @return a string representation of the bitset
+     */
     virtual std::string asString() const = 0;
 
 };
