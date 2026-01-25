030c00b49aeb2b468829df488878b27fdbba4aa5
Lucas Bucht <lmb290@pitt.edu>
2026-01-25 14:21:46 -0500

    Created default and integer constructors

--- project1/project1/BitArray.cpp
+++ project1/project1/BitArray.cpp
@@ -1,11 +1,29 @@
 #include "BitArray.hpp"
 
 BitArray::BitArray() {
-    // TODO
+    m_size = 8;
+    m_bits = new bool[8];
+    m_good = true;
+    for(intmax_t i = 0; i < m_size; i++){
+        m_bits[i] = false;
+    }
 }
 
 BitArray::BitArray(intmax_t size) {
-    // TODO
+    m_size = size;
+    m_bits = nullptr;
+    m_good = true;
+    
+    if (size <= 0){
+        m_good = false;
+        m_size = 0;
+        return;
+    }
+
+    m_bits = new bool[m_size];
+    for (intmax_t i = 0; i < m_size; i++){
+        m_bits[i] = false;
+    }
 }
 
 BitArray::BitArray(const std::string & value) {
