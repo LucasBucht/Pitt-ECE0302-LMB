bd1510cb4d3fa4f4ceb324686d3710ddeb117a41
Lucas Bucht <lmb290@pitt.edu>
2026-02-01 12:31:45 -0500

    Added all test cases

--- problem01/bags/test_LimitedSizeBag.cpp
+++ problem01/bags/test_LimitedSizeBag.cpp
@@ -3,15 +3,75 @@
 
 #include "LimitedSizeBag.hpp"
 
+TEST_CASE("Bag starts empty", "[LimitedSizeBag]") {
+  LimitedSizeBag<int> bag;
 
-TEST_CASE("Calling all public members", "[LimitedSizeBag]"){
-  LimitedSizeBag<int> b;
+  REQUIRE(bag.isEmpty());
+  REQUIRE(bag.getCurrentSize() == 0);
+}
 
-  b.add(0);
-  b.remove(0);
-  b.isEmpty();
-  b.getCurrentSize();
-  b.clear();
-  b.getFrequencyOf(0);
-  b.contains(0);
+TEST_CASE("Add items to the bag", "[LimitedSizeBag]") {
+  LimitedSizeBag<int> bag;
+
+  REQUIRE(bag.add(10));
+  REQUIRE(bag.add(20));
+  REQUIRE(bag.add(10));
+
+  REQUIRE_FALSE(bag.isEmpty());
+  REQUIRE(bag.getCurrentSize() == 3);
+}
+
+TEST_CASE("Frequency and contains work correctly", "[LimitedSizeBag]") {
+  LimitedSizeBag<int> bag;
+
+  bag.add(1);
+  bag.add(2);
+  bag.add(1);
+  bag.add(1);
+
+  REQUIRE(bag.getFrequencyOf(1) == 3);
+  REQUIRE(bag.getFrequencyOf(2) == 1);
+  REQUIRE(bag.getFrequencyOf(3) == 0);
+
+  REQUIRE(bag.contains(1));
+  REQUIRE(bag.contains(2));
+  REQUIRE_FALSE(bag.contains(3));
+}
+
+TEST_CASE("Remove items from the bag", "[LimitedSizeBag]") {
+  LimitedSizeBag<int> bag;
+
+  bag.add(5);
+  bag.add(6);
+  bag.add(5);
+
+  REQUIRE(bag.remove(5));
+  REQUIRE(bag.getCurrentSize() == 2);
+  REQUIRE(bag.getFrequencyOf(5) == 1);
+
+  REQUIRE_FALSE(bag.remove(42)); // item not present
+}
+
+TEST_CASE("Clear removes all items", "[LimitedSizeBag]") {
+  LimitedSizeBag<int> bag;
+
+  bag.add(1);
+  bag.add(2);
+  bag.add(3);
+
+  bag.clear();
+
+  REQUIRE(bag.isEmpty());
+  REQUIRE(bag.getCurrentSize() == 0);
+}
+
+TEST_CASE("Bag does not exceed max size", "[LimitedSizeBag]") {
+  LimitedSizeBag<int> bag;
+
+  for (std::size_t i = 0; i < LimitedSizeBag<int>::maxsize; i++){
+    REQUIRE(bag.add(static_cast<int>(i)));
+  }
+
+  REQUIRE(bag.getCurrentSize() == LimitedSizeBag<int>::maxsize);
+  REQUIRE_FALSE(bag.add(999));
 }
